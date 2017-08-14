5a3b95932134424bc9be38c6157e06b9a6687503
lordmaulwurf <tv@in-hd.de>
2017-08-14 17:44:40 +0200

    introduces new programs and changes

---
+++ Python/map_eval_2.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import yaml
+from bokeh.plotting import figure, show, output_file
+
+with open("spec_config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+dimensions = cfg["map_paras"]["dimensions"]
+background = cfg["map_paras"]["background"]
+raw = np.loadtxt(cfg["map_paras"]["file"])
+
+data = raw[6:]
+
+data3d = np.stack(np.vsplit(np.transpose(data)[1:],dimensions))-background
+
+
+print(np.shape(np.transpose(data)[1:]))
+
+print(np.shape(data3d))
+
+z = np.sum(data3d,axis=2) 
+z = z/np.max(z)
+print(np.shape(z))
+
+#fig = plt.figure()
+#ax = fig.add_subplot(111)
+#ax.set_title('Intensity Map')
+#hover = HoverTool(tooltips=[
+#    ("index", "$index"),
+#    ("(x,y)", "$x, $y"),
+#])
+
+p = figure(x_range=(0,dimensions), y_range=(0,dimensions), tools=['tap'])
+
+p.image(image=[z], x=0, y=0, dw=dimensions, dh=dimensions, palette="Inferno256")
+
+output_file("spectralmap.html", title="Spectral Map")
+
+show(p)
+
